d935baa3865f8a41d365b110224fd88648e55475
xy3 <a@xsq.pw>
2020-06-04 21:00:15 +0100

    icon

--- js/background.js
+++ js/background.js
@@ -1,22 +1,6 @@
-// chrome.browserAction.onClicked.addListener(function(activeTab){
-//   var newURL = "popup.html";
-//   chrome.tabs.create({ url: newURL });
-// });
-
-// chrome.browserAction.onClicked.addListener(function(tab) {
-// 	chrome.windows.create({
-// 		url: chrome.runtime.getURL("popup.html"),
-// 		type: "popup"
-// 	}, function(win) {
-//     // win represents the Window object from windows API
-//     // Do something after opening
-// });
-// });
-
 window.content = ""
 
 function read_this(info, tab) {
-	// console.log(info.selectionText)
 	window.content = info.selectionText
 	chrome.windows.create({
 		url: chrome.runtime.getURL("popup.html"),
